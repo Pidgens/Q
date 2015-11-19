3107a2d7d71d811239bfea3f61e5f9d5f55be1fe
pidgens <dederekchiu@berkeley.edu>
2015-11-19 11:44:23 -0800

    first

---
+++ wear/src/main/java/com/example/derekchiu/q/MyStubBroadcastActivity.java
@@ -0,0 +1,21 @@
+package com.example.derekchiu.q;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("com.example.derekchiu.q.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
