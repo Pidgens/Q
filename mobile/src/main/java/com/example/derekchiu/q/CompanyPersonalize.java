3052e00551652f610f188ab0963419bfcdf5fa93
Tomo Ueda <tueda14@gmail.com>
2015-12-01 18:55:16 -0800

    Created Watch/Phone sync

--- mobile/src/main/java/com/example/derekchiu/q/CompanyPersonalize.java
+++ mobile/src/main/java/com/example/derekchiu/q/CompanyPersonalize.java
@@ -3,28 +3,86 @@ package com.example.derekchiu.q;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
 
 /**
  * Created by derekchiu on 11/30/15.
  */
-public class CompanyPersonalize extends Activity {
+public class CompanyPersonalize extends Activity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     Button next;
+    String WEARABLE_COMPANY_PATH = "/wearable_company";
+    GoogleApiClient googleClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceBundle) {
         super.onCreate(savedInstanceBundle);
         setContentView(R.layout.cp_personalize);
+        googleClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
 
         next = (Button) findViewById(R.id.nextButton);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(CompanyPersonalize.this, JobManage.class);
+                DataMap notifyWearable = new DataMap();
+                new SendToDataLayerThread(WEARABLE_COMPANY_PATH, notifyWearable).start();
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+    }
+
+    class SendToDataLayerThread extends Thread {
+        String path;
+        DataMap dataMap;
+
+        // Constructor for sending data objects to the data layer
+        SendToDataLayerThread(String p, DataMap data) {
+            path = p;
+            dataMap = data;
+        }
+
+        public void run() {
+            // Construct a DataRequest and send over the data layer
+            PutDataMapRequest putDMR = PutDataMapRequest.create(path);
+            putDMR.getDataMap().putAll(dataMap);
+            PutDataRequest request = putDMR.asPutDataRequest();
+            DataApi.DataItemResult result = Wearable.DataApi.putDataItem(googleClient, request).await();
+            if (result.getStatus().isSuccess()) {
+                Log.v("myTag", "DataMap: " + dataMap + " sent successfully to data layer ");
+            }
+            else {
+                // Log an error
+                Log.v("myTag", "ERROR: failed to send DataMap to data layer");
+            }
+        }
     }
 }
--- wear/src/main/java/com/example/derekchiu/q/ChooseFlowActivity.java
+++ wear/src/main/java/com/example/derekchiu/q/ChooseFlowActivity.java
@@ -1,14 +1,24 @@
 package com.example.derekchiu.q;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.WearableListenerService;
 
 /**
  * Created by tomo on 12/1/15.
@@ -16,6 +26,8 @@ import android.widget.TextView;
 public class ChooseFlowActivity extends Activity {
     TextView view;
     Context context;
+
+    String WEARABLE_COMPANY_PATH = "/wearable_company";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,5 +61,41 @@ public class ChooseFlowActivity extends Activity {
             }
         });
         return view;
+    }
+
+    public class MobileConnector extends WearableListenerService {
+        @Override
+        public void onDataChanged(DataEventBuffer dataEvents) {
+            DataMap dataMap;
+            for (DataEvent event : dataEvents) {
+                // Check the event type
+                if (event.getType() == DataEvent.TYPE_CHANGED) {
+                    // Check the data path
+                    if (event.getDataItem().getUri().getPath().equals(WEARABLE_COMPANY_PATH)) {
+                        // Create a local notification
+                        dataMap = DataMapItem.fromDataItem(event.getDataItem()).getDataMap();
+                        sendLocalNotification(dataMap);
+                    }
+                }
+            }
+        }
+    }
+
+    private void sendLocalNotification(DataMap dataMap) {
+        int notificationId = 001;
+
+        Intent startIntent = new Intent(this, PeopleNamesActivity.class)
+                .setAction(Intent.ACTION_MAIN);
+        PendingIntent startPendingIntent =
+                PendingIntent.getActivity(this, 0, startIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        Notification notify = new NotificationCompat.Builder(this)
+                .setContentTitle("Open Q!")
+                .setAutoCancel(true)
+                .setContentIntent(startPendingIntent)
+                .build();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(notificationId, notify);
     }
 }
