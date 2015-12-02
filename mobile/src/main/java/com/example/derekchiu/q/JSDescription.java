34bb81d4ff6168fa25bb30fead34a91271d19fbf
Pidgens <dederekchiu@berkeley.edu>
2015-12-01 20:19:06 -0800

    more style changes

--- mobile/src/main/java/com/example/derekchiu/q/JSDescription.java
+++ mobile/src/main/java/com/example/derekchiu/q/JSDescription.java
@@ -2,16 +2,20 @@ package com.example.derekchiu.q;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by derekchiu on 12/1/15.
  */
 public class JSDescription extends Activity {
 
+    TextView welcome_msg;
+    Button nextButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceBundle) {
         super.onCreate(savedInstanceBundle);
         setContentView(R.layout.manage_page);
-
     }
 }
