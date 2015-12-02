8f2ac6ecc3b8a85fa4f6c35bd1e5001ff2d713b0
Pidgens <dederekchiu@berkeley.edu>
2015-12-01 23:01:21 -0800

    changes to front end

--- mobile/src/main/java/com/example/derekchiu/q/EnterQueue.java
+++ mobile/src/main/java/com/example/derekchiu/q/EnterQueue.java
@@ -2,15 +2,39 @@ package com.example.derekchiu.q;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by derekchiu on 12/1/15.
  */
 public class EnterQueue extends Activity {
+
+    ListView lv;
+    Button joinButton;
     @Override
     protected void onCreate(Bundle savedInstanceBundle) {
         super.onCreate(savedInstanceBundle);
         setContentView(R.layout.cp_description);
 
+        lv = (ListView) findViewById(R.id.listView);
+        joinButton = (Button) findViewById(R.id.joinButton);
+
+        ArrayList<String> companies =  new ArrayList<String>();
+        companies.add("Business Analyst");
+        ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, companies);
+        lv.setAdapter(listAdapter);
+
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                joinButton.setText("Entered Queue!");
+            }
+        });
     }
 }
--- mobile/src/main/java/com/example/derekchiu/q/JobManage.java
+++ mobile/src/main/java/com/example/derekchiu/q/JobManage.java
@@ -4,7 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by derekchiu on 11/30/15.
@@ -12,20 +18,42 @@ import android.widget.ImageView;
 public class JobManage extends Activity {
 
     ImageView company_descrip;
+    ListView lv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceBundle) {
         super.onCreate(savedInstanceBundle);
         setContentView(R.layout.manage_page);
 
-        company_descrip = (ImageView) findViewById(R.id.manageImage);
-        company_descrip.setOnClickListener(new View.OnClickListener() {
+        lv2 = (ListView) findViewById(R.id.lvJSList);
+        ArrayList<String> jobseekersList = new ArrayList<String>();
+        jobseekersList.add("John Doe");
+        jobseekersList.add("Tomo Ueda");
+        jobseekersList.add("Patrick Mc Gartoll");
+        jobseekersList.add("Derek Chiu");
+        jobseekersList.add("Roy Kim");
+        jobseekersList.add("Daniel He");
+        ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, jobseekersList);
+        lv2.setAdapter(listAdapter);
+
+        lv2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent descrip_page = new Intent(JobManage.this, CompanyDescription.class);
-                startActivity(descrip_page);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent i = new Intent(JobManage.this, CompanyDescription.class);
+                    startActivity(i);
+                }
             }
         });
+
+//        company_descrip = (ImageView) findViewById(R.id.manageImage);
+//        company_descrip.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent descrip_page = new Intent(JobManage.this, CompanyDescription.class);
+//                startActivity(descrip_page);
+//            }
+//        });
     }
 
 }
