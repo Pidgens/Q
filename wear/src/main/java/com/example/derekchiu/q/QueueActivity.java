d804218b64e5ccdc40b55e5663843aeb7770bd86
Tomo Ueda <tueda14@gmail.com>
2015-12-06 23:51:06 -0800

    Wearable-Client.

--- wear/src/main/java/com/example/derekchiu/q/QueueActivity.java
+++ wear/src/main/java/com/example/derekchiu/q/QueueActivity.java
@@ -1,51 +1,31 @@
 package com.example.derekchiu.q;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
+import android.support.wearable.view.CardFragment;
+import android.support.wearable.view.FragmentGridPagerAdapter;
+import android.support.wearable.view.GridViewPager;
+import android.util.Log;
 import android.widget.ImageButton;
-
-import java.util.Queue;
-import java.util.Timer;
+import android.widget.LinearLayout;
 
 /**
  * Created by tomo on 11/30/15.
  */
 public class QueueActivity extends Activity {
 
-    ImageButton view;
-    Context context;
+    GridViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        context = this;
-        setContentView(getMainView());
-    }
-
-    private ImageButton getMainView() {
-        view = new ImageButton(this);
-        view.setImageResource(R.drawable.queue);
-        view.setBackgroundColor(Color.WHITE);
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent drop = new Intent(context, DropActivity.class);
-                startActivity(drop);
-            }
-        });
-        view.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Intent timer = new Intent(context, TimerActivity.class);
-                startActivity(timer);
-                return true;
-            }
-        });
-        return view;
+        setContentView(R.layout.queue_layout);
+        pager = (GridViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new QueueGridPagerAdapter(this, getFragmentManager()));
     }
 
 }
